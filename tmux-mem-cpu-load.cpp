120416393630698dc6ce25832270095d83e13ca0
Justin Crawford <justinc@pci-online.net>
2013-04-15 11:35:38 -0700

    Fix the formatting to use tabs instead of spaces, easier to read this way.

--- tmux-mem-cpu-load.cpp
+++ tmux-mem-cpu-load.cpp
@@ -82,291 +82,171 @@ host_cpu_load_info_data_t _get_cpu_percentage()
 
 float cpu_percentage( unsigned int cpu_usage_delay )
 {
-  std::string stat_line;
-  size_t line_start_pos;
-  size_t line_end_pos;
-  unsigned long long current_user;
-  unsigned long long current_system;
-  unsigned long long current_nice;
-  unsigned long long current_idle;
-  unsigned long long next_user;
-  unsigned long long next_system;
-  unsigned long long next_nice;
-  unsigned long long next_idle;
-  unsigned long long diff_user;
-  unsigned long long diff_system;
-  unsigned long long diff_nice;
-  unsigned long long diff_idle;
-  std::istringstream iss;
+	std::string stat_line;
+	size_t line_start_pos;
+	size_t line_end_pos;
+	unsigned long long current_user;
+	unsigned long long current_system;
+	unsigned long long current_nice;
+	unsigned long long current_idle;
+	unsigned long long next_user;
+	unsigned long long next_system;
+	unsigned long long next_nice;
+	unsigned long long next_idle;
+	unsigned long long diff_user;
+	unsigned long long diff_system;
+	unsigned long long diff_nice;
+	unsigned long long diff_idle;
+	std::istringstream iss;
 
-  std::ifstream stat_file("/proc/stat");
-  std::getline(stat_file, stat_line);
-  stat_file.close();
+	std::ifstream stat_file("/proc/stat");
+	std::getline(stat_file, stat_line);
+	stat_file.close();
 
-  // skip "cpu"
-  line_start_pos = stat_line.find_first_not_of(" ", 3);
-  line_end_pos = stat_line.find_first_of(' ', line_start_pos);
-  line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
-  line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
-  line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
+	// skip "cpu"
+	line_start_pos = stat_line.find_first_not_of(" ", 3);
+	line_end_pos = stat_line.find_first_of(' ', line_start_pos);
+	line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
+	line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
+	line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
 
-  iss.str( stat_line.substr( line_start_pos, line_end_pos - line_start_pos ) );
-  iss >> current_user >> current_nice >> current_system >> current_idle;
-  iss.clear();
+	iss.str( stat_line.substr( line_start_pos, line_end_pos - line_start_pos ) );
+	iss >> current_user >> current_nice >> current_system >> current_idle;
+	iss.clear();
 
-  usleep( cpu_usage_delay );
+	usleep( cpu_usage_delay );
 
-  stat_file.open("/proc/stat");
-  std::getline(stat_file, stat_line);
-  stat_file.close();
+	stat_file.open("/proc/stat");
+	std::getline(stat_file, stat_line);
+	stat_file.close();
 
-  // skip "cpu"
-  line_start_pos = stat_line.find_first_not_of(" ", 3);
-  line_end_pos = stat_line.find_first_of(' ', line_start_pos);
-  line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
-  line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
-  line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
+	// skip "cpu"
+	line_start_pos = stat_line.find_first_not_of(" ", 3);
+	line_end_pos = stat_line.find_first_of(' ', line_start_pos);
+	line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
+	line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
+	line_end_pos = stat_line.find_first_of(' ', line_end_pos + 1);
 
-  iss.str( stat_line.substr( line_start_pos, line_end_pos - line_start_pos ) );
-  iss >> next_user >> next_nice >> next_system >> next_idle;
-  iss.clear();
+	iss.str( stat_line.substr( line_start_pos, line_end_pos - line_start_pos ) );
+	iss >> next_user >> next_nice >> next_system >> next_idle;
+	iss.clear();
 
-  diff_user   = next_user - current_user;
-  diff_system = next_system - current_system;
-  diff_nice   = next_nice - current_nice;
-  diff_idle   = next_idle - current_idle;
-#endif // platform
+	diff_user   = next_user - current_user;
+	diff_system = next_system - current_system;
+	diff_nice   = next_nice - current_nice;
+	diff_idle   = next_idle - current_idle;
 
-  return static_cast<float>(diff_user + diff_system + diff_nice)/static_cast<float>(diff_user + diff_system + diff_nice + diff_idle)*100.0;
+	return static_cast<float>(diff_user + diff_system + diff_nice)/static_cast<float>(diff_user + diff_system + diff_nice + diff_idle)*100.0;
 }
 
 std::string cpu_string( unsigned int cpu_usage_delay, unsigned int graph_lines )
 {
-  std::string meter( graph_lines + 2, ' ' );
-  meter[0] = '[';
-  meter[meter.length() - 1] = ']';
-  int meter_count = 0;
-  float percentage;
-  std::ostringstream oss;
-  oss.precision( 1 );
-  oss.setf( std::ios::fixed | std::ios::right );
+	std::string meter( graph_lines + 2, ' ' );
+	meter[0] = '[';
+	meter[meter.length() - 1] = ']';
+	int meter_count = 0;
+	float percentage;
+	std::ostringstream oss;
+	oss.precision( 1 );
+	oss.setf( std::ios::fixed | std::ios::right );
 
-  percentage = cpu_percentage( cpu_usage_delay );
-  float meter_step = 99.9 / graph_lines;
-  meter_count = 1;
-  while(meter_count*meter_step < percentage)
-    {
-    meter[meter_count] = '|';
-    meter_count++;
-    }
+	percentage = cpu_percentage( cpu_usage_delay );
+	float meter_step = 99.9 / graph_lines;
+	meter_count = 1;
+	while(meter_count*meter_step < percentage)
+	{
+		meter[meter_count] = '|';
+		meter_count++;
+	}
 
-  if( use_colors )
-    {
-    oss << cpu_percentage_lut[static_cast<unsigned int>( percentage )];
-    }
-  oss << meter;
-  oss.width( 5 );
-  oss << percentage;
-  oss << "%";
-  if( use_colors )
-    {
-    oss << "#[fg=default,bg=default]";
-    }
+	oss << meter;
+	oss.width( 5 );
+	oss << percentage;
+	oss << "%";
 
-  return oss.str();
+	return oss.str();
 }
 
 std::string mem_string()
 {
-  std::ostringstream oss;
-#if defined(BSD_BASED) || (defined(__APPLE__) && defined(__MACH__))
-// OSX or BSD based system, use BSD APIs instead
+	unsigned int total_mem;
+	unsigned int used_mem;
+	unsigned int unused_mem;
+	size_t line_start_pos;
+	size_t line_end_pos;
+	std::istringstream iss;
+	std::ostringstream oss;
+	std::string mem_line;
 
-#if defined(__APPLE__) && defined(__MACH__)
-  // These values are in bytes
-  int64_t total_mem;
-  int64_t used_mem;
-  int64_t unused_mem;
-  vm_size_t page_size;
-  mach_port_t mach_port;
-  mach_msg_type_number_t count;
-  vm_statistics_data_t vm_stats;
+	std::ifstream meminfo_file( "/proc/meminfo" );
+	std::getline( meminfo_file, mem_line );
+	line_start_pos = mem_line.find_first_of( ':' );
+	line_start_pos++;
+	line_end_pos = mem_line.find_first_of( 'k' );
+	iss.str( mem_line.substr( line_start_pos, line_end_pos - line_start_pos ) );
+	iss >> total_mem;
 
-  // Get total physical memory
-  int mib[2];
-  mib[0] = CTL_HW;
-  mib[1] = HW_MEMSIZE;
-  size_t length = sizeof(int64_t);
-  sysctl(mib, 2, &total_mem, &length, NULL, 0);
+	used_mem = total_mem;
 
-  mach_port = mach_host_self();
-  count = sizeof(vm_stats) / sizeof(natural_t);
-  if (KERN_SUCCESS == host_page_size(mach_port, &page_size) &&
-    KERN_SUCCESS == host_statistics(mach_port, HOST_VM_INFO, (host_info_t)&vm_stats, &count))
-    {
-    unused_mem = (int64_t)vm_stats.free_count * (int64_t)page_size;
+	for( unsigned int i = 0; i < 3; i++ )
+	{
+		std::getline( meminfo_file, mem_line );
+		line_start_pos = mem_line.find_first_of( ':' );
+		line_start_pos++;
+		line_end_pos = mem_line.find_first_of( 'k' );
+		iss.str( mem_line.substr( line_start_pos, line_end_pos - line_start_pos ) );
+		iss >> unused_mem;
+		used_mem -= unused_mem;
+	}
+	meminfo_file.close();
 
-    used_mem = ((int64_t)vm_stats.active_count +
-    (int64_t)vm_stats.inactive_count +
-    (int64_t)vm_stats.wire_count) *  (int64_t)page_size;
-    }
+	oss << used_mem / 1024 << '/' << total_mem / 1024 << "MB";
 
-  // To kilobytes
-#endif // Apple
-  // TODO BSD
-
-  used_mem /= 1024;
-  total_mem /= 1024;
-
-#else // Linux
-  unsigned int total_mem;
-  unsigned int used_mem;
-  unsigned int unused_mem;
-  size_t line_start_pos;
-  size_t line_end_pos;
-  std::istringstream iss;
-  std::ostringstream oss;
-  std::string mem_line;
-
-  std::ifstream meminfo_file( "/proc/meminfo" );
-  std::getline( meminfo_file, mem_line );
-  line_start_pos = mem_line.find_first_of( ':' );
-  line_start_pos++;
-  line_end_pos = mem_line.find_first_of( 'k' );
-  iss.str( mem_line.substr( line_start_pos, line_end_pos - line_start_pos ) );
-  iss >> total_mem;
-
-  used_mem = total_mem;
-
-  for( unsigned int i = 0; i < 3; i++ )
-    {
-    std::getline( meminfo_file, mem_line );
-    line_start_pos = mem_line.find_first_of( ':' );
-    line_start_pos++;
-    line_end_pos = mem_line.find_first_of( 'k' );
-    iss.str( mem_line.substr( line_start_pos, line_end_pos - line_start_pos ) );
-    iss >> unused_mem;
-    used_mem -= unused_mem;
-    }
-  meminfo_file.close();
-
-#endif // platform
-
-  if( use_colors )
-    {
-    oss << mem_lut[(100 * used_mem) / total_mem];
-    }
-  oss << used_mem / 1024 << '/' << total_mem / 1024 << "MB";
-  if( use_colors )
-    {
-    oss << "#[fg=default,bg=default]";
-    }
-
-  return oss.str();
+	return oss.str();
 }
 
 std::string load_string()
 {
-  std::ifstream loadavg_file( "/proc/loadavg" );
-  std::string load_line;
-  std::getline( loadavg_file, load_line );
-  loadavg_file.close();
+	std::ifstream loadavg_file( "/proc/loadavg" );
+	std::string load_line;
+	std::getline( loadavg_file, load_line );
+	loadavg_file.close();
 
-#endif // platform
-
-  if( use_colors )
-    {
-    std::ifstream stat_file( "/proc/stat" );
-    std::string stat_line;
-    std::getline( stat_file, stat_line );
-    // Likely does not work on BSD, but not tested
-    unsigned int number_of_cpus = sysconf( _SC_NPROCESSORS_ONLN );
-
-    std::istringstream iss( load_line.substr( 0, 4 ) );
-    float recent_load;
-    iss >> recent_load;
-    // colors range from zero to twice the number of cpu's for the most recent
-    // load metric
-    unsigned int load_percent = static_cast< unsigned int >( recent_load / number_of_cpus * 0.5f * 100.0f );
-    if( load_percent > 100 )
-      {
-      load_percent = 100;
-      }
-    oss << load_lut[load_percent];
-    }
-
-  oss << load_line.substr( 0, 14 );
-  if( use_colors )
-    {
-    oss << "#[fg=default,bg=default]";
-    }
-
-  return oss.str();
+	return load_line.substr( 0, 14 );
 }
 
 
 int main(int argc, char** argv)
 {
-  unsigned int cpu_usage_delay = 900000;
-  int graph_lines = 10;
-  bool use_colors = false;
-  try
-  {
-  std::istringstream iss;
-  iss.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
-  if( argc > 1 )
-    {
-    std::istringstream iss;
-    iss.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
-    std::string current_arg;
-    unsigned int arg_index = 1;
-    if( argc > arg_index )
-      {
-      if( strcmp( argv[arg_index], "--colors" ) == 0 )
-        {
-        use_colors = true;
-        ++arg_index;
-        }
-      }
-    if( argc > arg_index )
-      {
-      iss.str( argv[arg_index] );
-      int status_interval;
-      iss >> status_interval;
-      if( status_interval < 1 )
-        {
-        std::cerr << "Status interval argument must be one or greater." << std::endl;
-        return EXIT_FAILURE;
-        }
-      cpu_usage_delay = status_interval * 1000000 - 100000;
-      ++arg_index;
-      }
-    if( argc > arg_index )
-      {
-      iss.str( argv[arg_index] );
-      iss.clear();
-      iss >> graph_lines;
-      if( graph_lines < 1 )
-        {
-        std::cerr << "Graph lines argument must be one or greater." << std::endl;
-        return EXIT_FAILURE;
-        }
-      }
-    }
-  catch(const std::exception &e)
-    {
-    std::cerr << "Usage: " << argv[0] << " [--colors] [tmux_status-interval(seconds)] [graph lines]" << std::endl;
-    return EXIT_FAILURE;
-    }
-  }
-  catch(const std::exception &e)
-  {
-    std::cerr << "Usage: " << argv[0] << " [tmux_status-interval(seconds)] [graph lines]" << std::endl;
-    return 1;
-  }
+	unsigned int cpu_usage_delay = 900000;
+	unsigned int graph_lines = 10;
+	try
+	{
+		std::istringstream iss;
+		iss.exceptions ( std::ifstream::failbit | std::ifstream::badbit );
+		if( argc > 1 )
+		{
+			iss.str( argv[1] );
+			unsigned int status_interval;
+			iss >> status_interval;
+			cpu_usage_delay = status_interval * 1000000 - 100000;
+		}
 
-  std::cout << mem_string( use_colors ) << ' ' << cpu_string( cpu_usage_delay, graph_lines, use_colors ) << ' ' << load_string( use_colors );
+		if( argc > 2 )
+		{
+			iss.str( argv[2] );
+			iss.clear();
+			iss >> graph_lines;
+		}
+	}
+	catch(const std::exception &e)
+	{
+		std::cerr << "Usage: " << argv[0] << " [tmux_status-interval(seconds)] [graph lines]" << std::endl;
+		return 1;
+	}
 
-  return EXIT_SUCCESS;
+	std::cout << mem_string() << ' ' << cpu_string( cpu_usage_delay, graph_lines ) << ' ' << load_string();
+
+	return 0;
 }
 
