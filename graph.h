b30bd82907fe7e26262cdce63fe81c6aaf8b6d59
l0ner <pwslts@gmail.com>
2014-04-19 22:25:53 +0000

    fixed typos in graph.{cc,h} and linux/cpu.h

--- graph.h
+++ graph.h
@@ -1,7 +1,7 @@
 #ifndef GRAPH_H_
 #define GRAPH_H_
 
-char * getGraphByPrecentage(unsigned, unsigned len = 10);
+char * getGraphByPercentage(unsigned, unsigned len = 10);
 char * getGraphByValue(unsigned, unsigned, unsigned len = 10);
 
 #endif
