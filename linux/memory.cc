7c883b924a4cb7868394f6ff4b8e9defc061f149
Pawel "l0ner" Soltys <pwslts@gmail.com>
2015-01-18 19:20:55 +0100

    restore parsing of /proc/meminfo to get memory usage
    See issue:
    https://github.com/l0ner/tmux-mem-cpu-load/commit/e7f2dd25e82c6ec6fc596e686c2f835f51e22eef#commitcomment-9327932
    
    Unfortunately we can't pull info about cached memory from sysinfo.
    Thus we need stick to /proc/meminfo parsing. See code for more comments.

--- linux/memory.cc
+++ linux/memory.cc
@@ -17,6 +17,7 @@
  */
 
 #include <sstream>
+#include <fstream>
 #include <sys/sysinfo.h>
 
 #include "memory.h"
@@ -25,22 +26,73 @@
 
 std::string mem_string( bool use_colors = false )
 {
+  using std::string;
+  using std::ifstream;
+  using std::stoi;
+
   std::ostringstream oss;
 
-  struct sysinfo sinfo;
-  sysinfo(&sinfo);
+  string line, substr;
+  size_t substr_start;
+  size_t substr_len;
 
-  unsigned int total_mem = MEGABYTES(sinfo.totalram);
-  unsigned int used_mem = total_mem - MEGABYTES(sinfo.freeram);
-  // we don't need this for now
-  //unsigned int unused_mem = MEGABYTES(sinfo.freeram);
+  unsigned int total_mem, used_mem;
+
+  /* Since linux uses some RAM for disk caching, the actuall used ram is lower 
+   * than what sysinfo(), top or free reports. htop reports the usage in a 
+   * correct way. The memory used for caching doesn't count as used, since it
+   * can be freed in any moment. Usually it hapens automatically, when an
+   * application requests memory.
+   * In order to calculate the ram that's actually used we need to use the 
+   * following formula:
+   *    total_ram - free_ram - buffered_ram - cached_ram
+   *
+   * example data, junk removed, with comments added:
+   *
+   * MemTotal:        61768 kB    old
+   * MemFree:          1436 kB    old
+   * MemAvailable     ????? kB    ??
+   * MemShared:           0 kB    old (now always zero; not calculated)
+   * Buffers:          1312 kB    old
+   * Cached:          20932 kB    old
+   * SwapTotal:      122580 kB    old
+   * SwapFree:        60352 kB    old
+   */
+
+  ifstream memory_info("/proc/meminfo");
+
+  while( getline( memory_info, line ) )
+  {
+    substr_start = 0;
+    substr_len = line.find_first_of( ':' );
+    substr = line.substr( substr_start, substr_len );
+    substr_start = line.find_first_not_of( " ", substr_len + 1 );
+    substr_len = line.find_first_of( 'k' ) - substr_start;
+    if( substr.compare( "MemTotal" ) == 0 )
+    {
+      // get total memory
+      total_mem = stoi( line.substr( substr_start, substr_len ) );
+    }
+    else if( substr.compare( "MemFree" ) == 0 )
+    {
+      used_mem = total_mem - stoi( line.substr( substr_start, substr_len ) );
+    }
+    else if( substr.compare( "Buffers" ) == 0 ||
+             substr.compare( "Cached" ) == 0 )
+    {
+      used_mem -= stoi( line.substr( substr_start, substr_len ) );
+    }
+  }
 
   if( use_colors )
   {
     oss << mem_lut[(100 * used_mem) / total_mem];
   }
 
-  oss << used_mem << '/' << total_mem << "MB";
+  // we want megabytes on output, but since the values already come as 
+  // kilobytes we need to divide them by 1024 only once, thus we use
+  // KILOBYTES
+  oss << KILOBYTES(used_mem) << '/' << KILOBYTES(total_mem) << "MB";
 
   if( use_colors )
   {
