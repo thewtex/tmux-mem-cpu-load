3e7ce969036749cd25bd084f3c66599a21e131be
Pawel "l0ner" Soltys <pwslts@gmail.com>
2015-01-20 02:57:14 +0100

    removed junk, reindentation to adhere to style guidelines

--- openbsd/error.h
+++ openbsd/error.h
@@ -1,5 +1,7 @@
-/*
+/* vim: tabstop=2 shiftwidth=2 expandtab textwidth=80 linebreak wrap
+ *
  * Copyright 2012 Matthew McCormick
+ * Copyright 2015 Pawel 'l0ner' Soltys
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -12,28 +14,12 @@
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
- * */
-// This file was Authored by Justin Crawford <Justasic@gmail.com>
-// Based on: github.com/freebsd/freebsd/blob/master/usr.bin/top/machine.c
-// Based on: Apple.cpp for load_string/mem_string and apple's documentation
+ */
 
-#ifndef BSD_METER_COMMON_H_
-#define BSD_METER_COMMON_H_
+#ifndef BSD_ERROR_H_
+#define BSD_ERROR_H_
 
 #include <iostream>
-
-// Memory Sizes
-#define KILOBYTES(x) ((x)/1024)
-#define MEGABYTES(x) (KILOBYTES((x))/1024)
-#define GIGABYTES(x) (MEGABYTES((x))/1024)
-
-// CPU percentages stuff
-#define CP_USER   0
-#define CP_NICE   1
-#define CP_SYS    2
-#define CP_INTR   3
-#define CP_IDLE   4
-#define CPUSTATES 5
 
 inline void error(const char * error) {
   std::cerr << error << std::endl;
