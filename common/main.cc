e232eef156d507d1b6148dbb2a5ab795e7d0f4a1
henry.potgieter <henry.potgieter@viscousbits.ca>
2023-03-27 11:57:01 -0400

    Change naming from --nerd-graph to --vertical-graph

--- common/main.cc
+++ common/main.cc
@@ -37,7 +37,7 @@
 
 std::string cpu_string( CPU_MODE cpu_mode, unsigned int cpu_usage_delay, unsigned int graph_lines,
     bool use_colors = false,
-    bool use_powerline_left = false, bool use_powerline_right = false, bool use_nerd_graph = false)
+    bool use_powerline_left = false, bool use_powerline_right = false, bool use_vert_graph = false)
 {
 
   float percentage;
@@ -80,10 +80,10 @@ std::string cpu_string( CPU_MODE cpu_mode, unsigned int cpu_usage_delay, unsigne
     }
   }
 
-  if( use_nerd_graph )
+  if( use_vert_graph )
   {
     oss << "▕";
-    oss << get_graph_nerd( unsigned( percentage ) );
+    oss << get_graph_vert( unsigned( percentage ) );
     oss << "▏";
   }
   else if( graph_lines > 0)
@@ -129,8 +129,8 @@ void print_help()
     << "\tUse powerline left symbols throughout the output, enables --colors\n"
     << "-q, --powerline-right\n"
     << "\tUse powerline right symbols throughout the output, enables --colors\n"
-    << "-n, --nerd-graph\n"
-    << "\tUse NerdFont symbols to render CPU graph as vertical bar chart\n"
+    << "-n, --vertical-graph\n"
+    << "\tUse vertical bar chart for CPU graph\n"
     << "-i <value>, --interval <value>\n"
     << "\tSet tmux status refresh interval in seconds. Default: 1 second\n"
     << "-g <value>, --graph-lines <value>\n"
@@ -152,7 +152,7 @@ int main( int argc, char** argv )
   bool use_colors = false;
   bool use_powerline_left = false;
   bool use_powerline_right = false;
-  bool use_nerd_graph = false;
+  bool use_vert_graph = false;
   MEMORY_MODE mem_mode = MEMORY_MODE_DEFAULT;
   CPU_MODE cpu_mode = CPU_MODE_DEFAULT;
 
@@ -166,7 +166,7 @@ int main( int argc, char** argv )
     { "colors", no_argument, NULL, 'c' },
     { "powerline-left", no_argument, NULL, 'p' },
     { "powerline-right", no_argument, NULL, 'q' },
-    { "nerd-graph", no_argument, NULL, 'n' },
+    { "vertical-graph", no_argument, NULL, 'v' },
     { "interval", required_argument, NULL, 'i' },
     { "graph-lines", required_argument, NULL, 'g' },
     { "mem-mode", required_argument, NULL, 'm' },
@@ -177,7 +177,7 @@ int main( int argc, char** argv )
 
   int c;
   // while c != -1
-  while( (c = getopt_long( argc, argv, "hi:cpqng:m:a:t:", long_options, NULL) ) != -1 )
+  while( (c = getopt_long( argc, argv, "hi:cpqvg:m:a:t:", long_options, NULL) ) != -1 )
   {
     switch( c )
     {
@@ -196,8 +196,8 @@ int main( int argc, char** argv )
         use_colors = true;
         use_powerline_right = true;
         break;
-      case 'n': // --nerd-graph
-        use_nerd_graph = true;
+      case 'v': // --vertical-graph
+        use_vert_graph = true;
         break;
       case 'i': // --interval, -i
         if( atoi( optarg ) < 1 )
@@ -260,7 +260,7 @@ int main( int argc, char** argv )
   MemoryStatus memory_status;
   mem_status( memory_status );
   std::cout << mem_string( memory_status, mem_mode, use_colors, use_powerline_left, use_powerline_right )
-    << cpu_string( cpu_mode, cpu_usage_delay, graph_lines, use_colors, use_powerline_left, use_powerline_right, use_nerd_graph )
+    << cpu_string( cpu_mode, cpu_usage_delay, graph_lines, use_colors, use_powerline_left, use_powerline_right, use_vert_graph )
     << load_string( use_colors, use_powerline_left, use_powerline_right, averages_count );
 
   std::cout << std::endl;
