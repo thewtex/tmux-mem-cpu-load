4cf0dad920a35e0d794f3b6f009b3e8303484a06
Robert Nagy <robert@openbsd.org>
2023-05-12 17:08:39 +0200

    use the correct CP_* definitions and number of CPUSTATES on OpenBSD

--- common/cpu.h
+++ common/cpu.h
@@ -28,12 +28,15 @@
   #define CP_IDLE 2
   #define CP_NICE 3
   #define CP_STATES 4
+#elif defined(__OpenBSD__)
+  #include <sys/sched.h>
+  #define CP_STATES CPUSTATES
 #else
   #define CP_USER   0
   #define CP_NICE   1
   #define CP_SYS    2
 
-  #if defined(__FreeBSD__) || defined(__NetBSD__) || defined(__OpenBSD__)
+  #if defined(__FreeBSD__) || defined(__NetBSD__)
     // *BSD or OSX
     #define CP_INTR   3
     #define CP_IDLE   4
